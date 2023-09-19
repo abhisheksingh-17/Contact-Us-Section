8acf30acf7a1267686d1df2cea67581e6387681d
abhisheksingh-17 <abhisheksingh1726@gmail.com>
2023-09-19 21:47:26 +0530

    Readme Commit

--- form.php
+++ form.php
@@ -4,16 +4,40 @@ if (isset($_POST['submit'])) {
     $email = $_POST['email'];
     $message = $_POST['message'];
 
-    $data = "Name: $name\nEmail: $email\nMessage: $message\n\n";
+    // Database connection details
+    $host = 'localhost';
+    $db_username = 'admin';
+    $db_password = '@abhi$17';
+    $db_name = 'contact_form';
 
-    // Specify the path to the text file where you want to store the data
-    $file = 'contact_data.txt';
+    // Connect to the database
+    $conn = new mysqli($host, $db_username, $db_password, $db_name);
 
-    // Open the file in append mode and save the data
-    file_put_contents($file, $data, FILE_APPEND);
+    // Check the connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
-    // Redirect to a thank you page after saving the data
-    header("Location: thankyou.html");
-    exit();
+    // Prepare and execute an SQL query to insert data
+    $sql = "INSERT INTO contact (name, email, message) VALUES (?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sss", $name, $email, $message);
+
+    if ($stmt->execute()) {
+        // Data inserted successfully
+        $stmt->close();
+        $conn->close();
+
+        // Redirect to a thank you page after saving the data
+        header("Location: thankyou.html");
+        exit();
+    } else {
+        // Error occurred
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    // Close the database connection
+    $stmt->close();
+    $conn->close();
 }
 ?>
